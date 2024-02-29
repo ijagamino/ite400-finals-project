a0ea99e5c59a81a89ea7274091d28044abf719a5
IJAGamino <ijagamino@gmail.com>
2024-02-29 13:59:40 +0800

    added some content in index (unfinished)

--- index.php
+++ index.php
@@ -2,13 +2,35 @@
 require_once 'bootstrap.php';
 include APPROOT.'/inc/header.php';
 ?>
-<!-- Content here -->
-<!-- #### Hero Image
+<!-- Featured product -->
+<div class="row">
+  <section class="shadow col-6 mx-auto pt-3">
+    <div class="row">
+      <div class="col-6 placeholder">
+        <img src="" alt="" class="">
+      </div>
+      <div class="col-6">
+        <header>
+          <h2>
+            Featured Product
+          </h2>
+        </header>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam et aperiam optio hic voluptate officia architecto quam illum possimus eveniet, quos amet, blanditiis soluta, culpa totam. Placeat ipsum ad deserunt.
+        </p>
+      </div>
+    </div>
+  </section>
+  <section clasc="col-6">
+    <div class="row">
+      <div class="col-auto mx-auto placeholder">
+        <img src="" alt="" class="rounded-circle">
+      </div>
+    </div>
+  </section>
+</div>
+<!-- Overview about us -->
 
-A mouthwatering photo showcasing your most popular cake or a beautiful arrangement of specialty cakes.</p>
+<!-- Overview contact -->
 
-#### Clear Value Proposition
-
-Briefly introduce your shop and highlight what makes it special. -->
-<!-- End of content -->
 <?php include APPROOT.'/inc/footer.php'; ?>
