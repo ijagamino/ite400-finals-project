e3bb2d4186b506e002eb2659f43dd7842011a36e
Ivan <ijagamino@gmail.com>
2024-03-05 18:46:03 +0800

    Menu page (#6)
    * initial layout for menu page
    
    * arrow is now also clickable
    
    * second iteration of layout

--- index.php
+++ index.php
@@ -14,10 +14,11 @@ include APPROOT.'/inc/header.php';
       <div class="d-flex justify-content-between flex-grow-1 align-items-center">
         <a href="<?php echo URLROOT; ?>/user/login.php"class="btn btn-lg btn-primary">Order now</a>
         <div class="">
-          <a href="<?php echo URLROOT; ?>/menu.php" class="link-underline link-underline-opacity-0">Browse menu</a>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
-          </svg>
+          <a href="<?php echo URLROOT; ?>/menu.php" class="link-underline link-underline-opacity-0">Browse menu
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
+            </svg>
+          </a>
         </div>
       </div>
     </div>
