4f828194adb5fc83934a1faa0781b825b9d7d872
ijagamino <ijagamino@gmail.com>
2024-03-05 20:42:18 +0800

    Added wrapper to main content for footer support

--- inc/footer.php
+++ inc/footer.php
@@ -1,4 +1,23 @@
-    </main>
+      </main>
+      <!-- <footer class="position-fixed start-0 end-0 bottom-0 py-3 bg-primary">
+        <div class="container text-center">
+          <span class="text-white">Copyright &copy; 2024 Cake Hub</span>
+          <div>
+            <a class="text-white" href="<?= URLROOT ?>/about.php">About Us</a>
+            <a class="text-white" href="<?= URLROOT ?>/contact.php">Contact Us</a>
+          </div>
+        </div>
+      </footer> -->
+      <footer class="py-3 bg-primary">
+        <div class="container text-center">
+          <span class="text-white">Copyright &copy; 2024 Cake Hub</span>
+          <div>
+            <a class="text-white" href="<?= URLROOT ?>/about.php">About Us</a>
+            <a class="text-white" href="<?= URLROOT ?>/contact.php">Contact Us</a>
+          </div>
+        </div>
+      </footer>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
--- inc/header.php
+++ inc/header.php
@@ -7,5 +7,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
+    <div class="d-flex flex-column justify-content-between vh-100">
     <?php include APPROOT.'/inc/navbar.php'?>
-    <main class="container py-3">
+      <main class="container py-3 mb-5">
