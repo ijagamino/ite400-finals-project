b96ca0e3bcf5f692febc0e1bb3697aab17722374
Ivan <ijagamino@gmail.com>
2024-03-07 11:10:08 +0800

    Admin pages initial design finished (#9)
    * added aside
    
    * initial layout for each pages
    
    * added aside
    
    * initial layout for each pages
    
    * added register page for admin

---
+++ inc/aside.php
@@ -0,0 +1,12 @@
+<aside class="col-3">
+  <nav class="">
+    <ul class="nav flex-column">
+      <li class="nav-item">
+        <a class="nav-link" href="<?= URLROOT?>/admin/index.php">Overview</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="<?= URLROOT?>/admin/profile.php">Profile</a>
+      </li>
+    </ul>
+  </nav>
+</aside>
